e32c911916873b000f1c94ef214e68fb5b677927
Viktorija Z <25870341+vzaboraite@users.noreply.github.com>
2021-08-10 19:13:36 +0100

    Create renderAppointmentsList(); use "dummy-data" to test rendered data.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
+// ELEMENTS
+
+const rootEl = document.querySelector("#root");
+console.log(rootEl);
+
+// STATE OBJECT
+
 let state = {
   jobs: [],
   appointments: [],
@@ -10,3 +17,96 @@ let state = {
 };
 
 console.log(state);
+
+//  DUMMY DATA
+const appointments = [
+  {
+    id: 1,
+    jobId: 1,
+    date: "2021-08-10",
+    time: "16:30",
+    job: {
+      id: 1,
+      company: "Barclays",
+      position: "Software Engineer",
+      salary: {
+        min: 60000,
+        max: 120000,
+      },
+      languages: ["javascript", "react"],
+      experienceLevel: "junior",
+      timeWhenPosted: 8,
+    },
+  },
+  {
+    id: 2,
+    jobId: 2,
+    date: "2021-08-10",
+    time: "16:30",
+    job: {
+      id: 2,
+      company: "Udemy",
+      position: "Frontend Engineer",
+      salary: {
+        min: 35000,
+        max: 60000,
+      },
+      languages: ["javascript", "react", "angular"],
+      experienceLevel: "junior",
+      timeWhenPosted: 19,
+    },
+  },
+];
+
+console.log(appointments);
+
+// RENDER FUNCTIONS
+
+function renderAppointmentsList(appointments) {
+  console.log("Inside renderAppointmentsList: ", appointments);
+
+  const ulEl = document.createElement("ul");
+  rootEl.append(ulEl);
+
+  appointments.forEach((appointment) => {
+    const appointmentCardEl = document.createElement("li");
+    appointmentCardEl.className = "appointment-card";
+    ulEl.append(appointmentCardEl);
+
+    const headingEl = document.createElement("h3");
+    headingEl.className = "appointment-heading";
+    headingEl.innerText = `${appointment.job.company}`;
+    appointmentCardEl.append(headingEl);
+
+    const dateWrapperEl = document.createElement("div");
+    appointmentCardEl.append(dateWrapperEl);
+
+    const dateSpanEl = document.createElement("span");
+    dateSpanEl.innerText = "Date: ";
+    dateWrapperEl.append(dateSpanEl);
+
+    const dateEl = document.createElement("span");
+    dateEl.className = "date-element";
+    dateEl.innerText = `${appointment.date}`;
+    dateWrapperEl.append(dateEl);
+
+    const timeWrapperEl = document.createElement("div");
+    appointmentCardEl.append(timeWrapperEl);
+
+    const timeSpanEl = document.createElement("span");
+    timeSpanEl.innerText = "Time: ";
+    timeWrapperEl.append(timeSpanEl);
+
+    const timeEl = document.createElement("span");
+    timeEl.className = "time-element";
+    timeEl.innerText = `${appointment.time}`;
+    timeWrapperEl.append(timeEl);
+
+    const cancelButtonEl = document.createElement("button");
+    cancelButtonEl.className = "cancel-button";
+    cancelButtonEl.innerText = "Cancel";
+    appointmentCardEl.append(cancelButtonEl);
+  });
+}
+
+renderAppointmentsList(appointments);
